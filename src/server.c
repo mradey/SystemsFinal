8034ddb0d4129b429def50a9e31d47de364d5fc4
Michael Radey <radeymichael@gmail.com>
2018-11-28 13:15:55 -0500

    commented server code

--- src/server.c
+++ src/server.c
@@ -23,17 +23,28 @@ int main(int argc, char *argv[])
       fprintf(stderr, "Need port number");
       exit(1);
      }
+     //file descriptors
+     int sockfd, newsockfd;
+     
+     //port number
+     int portno;
+     
+     //check if function successful
+     int n;
 
-     int sockfd, newsockfd, portno;
+     //message buffer and internet address for connection
      socklen_t clilen;
      char buffer[255];
      struct sockaddr_in serv_addr, cli_addr;
-     int n;
 
+     //TCP connection
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
      if (sockfd < 0) 
         error("ERROR opening socket");
+
+     //clear all text
      bzero((char *) &serv_addr, sizeof(serv_addr));
+     
      portno = atoi(argv[1]);
      serv_addr.sin_family = AF_INET;
      serv_addr.sin_addr.s_addr = INADDR_ANY;
@@ -41,11 +52,16 @@ int main(int argc, char *argv[])
      if (bind(sockfd, (struct sockaddr *) &serv_addr,
               sizeof(serv_addr)) < 0) 
               error("ERROR on binding");
+     
+     //listen for messages from max 5 clients
      listen(sockfd,5);
      clilen = sizeof(cli_addr);
+
+     //Connection accepted and new file descriptor
      newsockfd = accept(sockfd, 
                  (struct sockaddr *) &cli_addr, 
                  &clilen);
+
      if (newsockfd < 0) 
           error("ERROR on accept");
      while(1)
@@ -53,15 +69,25 @@ int main(int argc, char *argv[])
            bzero(buffer,256);
            n = read(newsockfd,buffer,255);
            if (n < 0) error("ERROR reading from socket");
+
+	   //display client message
            printf("Client: %s\n",buffer);
-          bzero(buffer,256);
-          fgets(buffer,255,stdin);
-          n = write(newsockfd,buffer,strlen(buffer));
+
+	   //clear all buffer
+           bzero(buffer,256);
+           fgets(buffer,255,stdin);
+
+	   //Let server write back
+           n = write(newsockfd,buffer,strlen(buffer));
+
            if (n < 0) error("ERROR writing to socket");
+
+	   //write Bye to end connection
            int i=strncmp("Bye" , buffer, 3);
            if(i == 0)
                break;
      }
+     //Close sockets
      close(newsockfd);
      close(sockfd);
      return 0; 

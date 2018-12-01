78510070f3c3642123e43dfb2fb81719c0d8101b
Mikebrag <Mikebrag@users.noreply.github.com>
2018-11-30 19:29:48 -0500

    Update lkm_msg.c
    commenting

--- src/lkm/lkm_msg.c
+++ src/lkm/lkm_msg.c
@@ -6,6 +6,7 @@
 #include <linux/fcntl.h>
 #include <asm/uaccess.h>
 
+// externall information about the module
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Michael, Kelly, Michael");
 MODULE_DESCRIPTION("IPC Kernel Module");
@@ -29,17 +30,20 @@ struct file *file_open(const char *path, int flags, int permissions)
     // error code
     int error = 0;
 
+    // get the x86 processor's FS segment register 
     fs = get_fs();
     set_fs(get_ds());
 
-    // actually open the file itself
+    // actually open the file itself with the given permissions
     filp = filp_open(path, flags, permissions);
+    // set the x86 processor's FS segment register
     set_fs(fs);
     // make sure there is no error with the file pointer returned
     if (IS_ERR(filp)) {
         error = PTR_ERR(filp);
         return NULL;
     }
+    // once the file pointer is created, return it
     return filp;
 }
 
@@ -48,26 +52,28 @@ int file_read(struct file *file, unsigned long offset, unsigned char *data, unsi
     mm_segment_t fs;
     int ret;
 
+    // set the x86 processor's FS segment register
     fs = get_fs();
     set_fs(get_ds());
     
-    // actually does the reading of the file since sys_read doesnt work in kernel
+    // actually does the reading of the file with the vfs functionality since sys_read doesnt work in kernel
     ret = vfs_read(file, data, size, &offset);
-
+    // set the x86 processor's FS segment register
     set_fs(fs);
     return ret;
 } 
 
 void file_close(struct file *file) 
 {
-    filp_close(file, NULL);
+	// close the file pointer and set the value to null
+	filp_close(file, NULL);
 }
 
 static int __init lkm_message_init(void)
 {
 	// print to the kernel space that the module loaded and the path given	
 	printk(KERN_INFO "Kernel module has been loaded\n");
-	// no path has been passed in 
+	// no path has been passed in, we are unable to read the messages 
 	if(strcmp(path,"") == 0)
 	{
 		printk(KERN_INFO "No file path passed in to open path to client");
@@ -90,6 +96,7 @@ static int __init lkm_message_init(void)
 		file_read(f, 0, buf, 100);
 		// print out the buffer to the kernel
 		printk(KERN_INFO "Client: %s\n", buf);
+		exit(0);
 	}
 	else
 	{
@@ -101,6 +108,7 @@ static int __init lkm_message_init(void)
 }
 
 static void __exit lkm_message_exit(void) {
+	// let the user know that the module has finished unloading
 	printk(KERN_INFO "Module unloaded\n");
 }
 
